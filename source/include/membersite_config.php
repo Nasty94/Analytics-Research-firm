d95c9983d1f2c127183d674a42b8f8dffc40ba49
Nasty94 <Nasty94@hot.ee>
2015-02-18 00:36:42 +0200

    project demo
    home oage loodud (ava index.html).
    Ma ei tea miks body borders lõpevad keskel.... (vt. style.css ->body)
    Ka PHPMailer ei tööta, seega ei toimu registreerimine korralikult...

---
+++ source/include/membersite_config.php
@@ -0,0 +1,26 @@
+<?PHP
+require_once("./include/fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('consultingLK.com');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('Nasty95@consultingLK.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'root',
+                      /*password*/'Y7toE85k',
+                      /*database name*/'test',
+                      /*table name*/'clients2');
+
+//For better security. Get a random string from this link: http://tinyurl.com/randstr
+// and put it here
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>
