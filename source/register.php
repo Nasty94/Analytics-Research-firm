5e2f522bdc510c9696335c67f2954550b4e97791
Anastassia <Nasty94@users.noreply.github.com>
2015-02-25 21:29:52 +0200

    Update register.php

--- source/register.php
+++ source/register.php
@@ -53,29 +53,39 @@ if(isset($_POST['submitted']))
 
 <div class="dropdownmenu">
         <ul id="nav">
-            <li class='active'><a href='index.html'>Avaleht</a></li>
-            <li><a href="#">Logi sisse</a>
+            <li class='active'><a href='#'>Avaleht</a></li>
+            <li><a href="#">Minu konto</a>
                 <div>
                     <ul>
-                        <li><a href='register.php'>Registreerimine</a></li>
-                        <li><a href='login.php'>Login</a></li>
+                        <li><a href='register.php'>Uus kasutaja</a></li>
+                        <li><a href='login.php'>Logi sisse</a></li>
                        
                     </ul>
                 </div>
             </li>
-            <li><a href="#">Meist</a>
+			<li><a href="#">Meist</a>
                 <div>
                     <ul>
                         <li><a href="#">Personal</a></li>
-                        <li><a href="#">Ettevõte</a></li>
+                        <li><a href="#">Concepts</a></li>
   
                     </ul>
                 </div>
             </li>
-            <li><a href="#">Artiklid</a>
+			<li><a href="#">Teenused</a>
                 <div>
                     <ul>
-                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Turuuring</a></li>
+                        <li><a href="#">Med. statistika</a></li>
+  
+                    </ul>
+                </div>
+            </li>
+            
+            <li><a href="#">Blog</a>
+                <div>
+                    <ul>
+                        <li><a href="arcticle_sample.php">Näidis</a></li>
                         <li><a href="#">Page 2</a></li>
                         <li><a href="#">Page 3</a></li>
                         <li><a href="#">Page 4</a></li>
@@ -83,8 +93,8 @@ if(isset($_POST['submitted']))
                     </ul>
                 </div>
             </li>
-            <li><a href="#">Muu</a></li>
-            <li><a href="#">KKK</a></li>
+            <li><a href="contact.php">  Kontakt  </a></li>
+			<li><a href="#">           </a></li>
 			
             <li class="pad"></li>
         </ul>
