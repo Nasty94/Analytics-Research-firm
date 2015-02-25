b53c8622beafd0bc73f078299285f8eb106bb5ca
Anastassia <Nasty94@users.noreply.github.com>
2015-02-25 14:25:23 +0200

    Update post_comment.php

--- source/post_comment.php
+++ source/post_comment.php
@@ -5,9 +5,9 @@ $db = new Persistence();
 $added = $db->add_comment($_POST);
 
 if($added) {
-  header( 'Location: index.php' );
+  header( 'Location: arcticle_sample.php' );
 }
 else {
-  header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  header( 'Location: arcticle_sample.php?error=Your comment was not posted due to errors in your form submission' );
 }
 ?>
