f9f9953b8b122326123377fe60af586dda2f7314
Anastassia <Nasty94@users.noreply.github.com>
2015-02-25 19:34:26 +0200

    Update arcticle_sample.php

--- source/arcticle_sample.php
+++ source/arcticle_sample.php
@@ -89,7 +89,7 @@ if(!$fgmembersite->CheckLogin())
             <li><a href="#">Blog</a>
                 <div>
                     <ul>
-                        <li><a href="arcticle_sample.php">Page 1</a></li>
+                        <li><a href="arcticle_sample.php">Näidis</a></li>
                         <li><a href="#">Page 2</a></li>
                         <li><a href="#">Page 3</a></li>
                         <li><a href="#">Page 4</a></li>
