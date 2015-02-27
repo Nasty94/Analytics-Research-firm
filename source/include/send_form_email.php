c0bb6d81cc91153384c1357a80fb080e8049b817
Nasty94 <Nasty94@hot.ee>
2015-02-27 19:41:16 +0200

    contact done

--- source/include/send_form_email.php
+++ source/include/send_form_email.php
@@ -138,13 +138,150 @@ $headers = 'From: '.$email_from."\r\n".
  
 ?>
  
- 
- 
-<!-- include your own success html here -->
- 
- 
- 
-Thank you for contacting us. We will be in touch with you very soon.
+<?PHP
+require_once("./include/membersite_config.php");
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->RegisterUser())
+   {
+        $fgmembersite->RedirectToURL("thank-you.html");
+   }
+}
+
+?>
+
+<!DOCTYPE html >
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="et">
+<head>
+    <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
+	<meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Äitah!</title>
+     
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+    <script type='text/javascript' src='../scripts/gen_validatorv31.js'></script>
+	<script src="../js/disable.js"></script>
+    <script src="scripts/pwdwidget.js" type="text/javascript"></script>   
+	
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
+	<link rel="stylesheet" href="../style/menubar.css">
+	<link rel="stylesheet" href="../style/style.css">
+	<link rel="stylesheet" href="../style/verticalmenu.css">
+	<link rel="STYLESHEET" type="text/css" href="../style/pwdwidget.css" />
+    <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite.css" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite_form.css" />
+        
+    	
+</head>
+	<body> 
+			
+	      <div id="left"></div>
+		  <div id="right"></div>
+		  <div id="top"></div>
+		  <div id="bottom"></div>
+		  
+		  <div id="english">
+		  <br>
+		  <a id="eng" href="../web/lang/eng/index_eng.html">ENG</a>&nbsp;&nbsp;
+	
+		  </div>
+		  <div id="img">
+          
+		  <img src="../img/LK.jpg" width=auto height=auto>
+		  </div> <!-- img -->
+		  
+       <div class="dropdownmenu">
+        <ul id="nav">
+            <li class='active'><a href='../index.html'>Avaleht</a></li>
+            <li><a href="#">Minu konto</a>
+                <div>
+                    <ul>
+                        <li><a href='../register.php'>Uus kasutaja</a></li>
+                        <li><a href='../login.php'>Logi sisse</a></li>
+                       
+                    </ul>
+                </div>
+            </li>
+			<li><a href="#">Meist</a>
+                <div>
+                    <ul>
+                        <li><a href="#">Personal</a></li>
+                        <li><a href="#">Concepts</a></li>
+  
+                    </ul>
+                </div>
+            </li>
+			<li><a href="#">Teenused</a>
+                <div>
+                    <ul>
+                        <li><a href="#">Turuuring</a></li>
+                        <li><a href="#">Med. statistika</a></li>
+  
+                    </ul>
+                </div>
+            </li>
+            
+            <li><a href="#">Blog</a>
+                <div>
+                    <ul>
+                        <li><a href="../web/articles/article-sample.php">Artikkel 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
+                        <li><a href="#">Page 4</a></li>
+                        <li><a href="#">Page 5</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="#">           </a></li>
+			<li><a href="#">           </a></li>
+			<li><a href="#">           </a></li>
+			<li><a href="#">           </a></li>
+			<li><a href="#">           </a></li>
+			
+            <li class="pad"></li>
+        </ul>
+    </div>
+	  
+
+		   		   
+<div id="main" style="height:1000px">
+	
+
+<div id="white-box" >
+<div class="center">
+    Äitah! Vastem Teile tellimusele esimesel võimalusel!
+
+</div><!--center-->
+
+
+<div id="contentInt">
+                 <noscript>
+                        <p class="note">You have disabled Javascript. This website will not function without it.</p>
+                 </noscript>                  
+
+
+</div><!--contentInt-->
+	
+ </div>  <!-- fg_membersite_form -->   		   
+	</div> <!-- white box --> 
+	</div> <!-- main -->  
+
+		  
+		  <div id="footer">
+                      © 2015  LK Consulting <br>
+					  This is a proof-of-concept web application.
+		  </div>
+
+
+		  	
+	</body>
+</html>
+
  
  
  
