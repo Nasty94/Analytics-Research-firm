591275c83112c4fbc1425327e3a07126e5a4e021
Nasty94 <Nasty94@hot.ee>
2015-02-27 19:47:41 +0200

    contact done

--- source/include/send_form_email.php
+++ source/include/send_form_email.php
@@ -158,8 +158,9 @@ if(isset($_POST['submitted']))
 	<meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	
     <meta content="utf-8" http-equiv="encoding">
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <title>Äitah!</title>
      
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
