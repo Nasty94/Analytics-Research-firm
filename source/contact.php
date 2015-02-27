e2a2d0f2ea439241a466bd5420510abe84647906
Nasty94 <Nasty94@hot.ee>
2015-02-27 18:19:56 +0200

    contact

--- source/contact.php
+++ source/contact.php
@@ -141,6 +141,46 @@ if(isset($_POST['submitted']))
 
                </FORM>
 			   <FORM name="subform" id="FORM">
+			   		   <div id='fg_membersite_form'>
+<form id='register' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Minu tellimus</legend>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+<input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='name' >Nimi*: </label><br/>
+    <input type='text' name='name' id='name' value='<?php echo $fgmembersite->SafeDisplay('name') ?>' maxlength="50" /><br/>
+    <span id='register_name_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='email' >Email*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='register_email_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='phone_number' >Telefoninumber:</label><br/>
+    <input type='text' name='phone_number' id='phone_number' value='<?php echo $fgmembersite->SafeDisplay('phone_number') ?>' maxlength="50" /><br/>
+    
+</div>
+
+<div class='container'>
+    <label for="order" >Tellimuse kirjeldus:</label><br/>
+    <textarea name='order' id='order' maxlength="2000" rows="3" required="required"></textarea>   
+    <div id='register_password_errorloc' class='error' style='clear:both'></div>
+</div>
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>	
+</div>
                     <br>
 			   		<div class="center">
 					
