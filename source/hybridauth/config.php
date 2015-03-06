47a1d4af121e724d0ead9d98389e75985b9f4df9
Markusli <markus.lippus@gmail.com>
2015-03-06 20:21:33 +0200

    Change in hybridauth config file to not look for stuff at localhost

--- source/hybridauth/config.php
+++ source/hybridauth/config.php
@@ -11,7 +11,7 @@
 
 return 
 	array(
-		"base_url" => "http://localhost/hybridauth/", 
+		"base_url" => "http://lk-consulting.azurewebsites.net/source/hybridauth/", 
 
 		"providers" => array ( 
 			// openid providers
