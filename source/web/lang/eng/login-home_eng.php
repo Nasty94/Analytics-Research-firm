a0313ea98f8619fe6bf31388a63db80dc603acd4
Anastassia <Nasty94@users.noreply.github.com>
2015-03-07 23:14:03 +0200

    add company.html and staff.html

--- source/web/lang/eng/login-home_eng.php
+++ source/web/lang/eng/login-home_eng.php
@@ -12,13 +12,12 @@ if(!$fgmembersite->CheckLogin())
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
     <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
-	<meta charset='utf-8'>
+    <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My account</title>
      
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="script.js"></script>	
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script type='text/javascript' src='../../../scripts/gen_validatorv31.js'></script>
     <script src="../../../scripts/pwdwidget.js" type="text/javascript"></script>   
 	
@@ -27,7 +26,7 @@ if(!$fgmembersite->CheckLogin())
 	<link rel="stylesheet" href="../../../style/style.css">
 	<link rel="stylesheet" href="../../../style/verticalmenu.css">
 	<link rel="STYLESHEET" type="text/css" href="../../../style/pwdwidget.css" />
-    <link rel="STYLESHEET" type="text/css" href="../../../style/fg_membersite.css" />
+        <link rel="STYLESHEET" type="text/css" href="../../../style/fg_membersite.css" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         
     	
@@ -54,7 +53,7 @@ if(!$fgmembersite->CheckLogin())
                     <ul>
                         
                         <li><a href='../../../change-pwd.php'>Change password</a></li>
-						<li><a href='logout_eng.php'>Log out</a></li>
+			<li><a href='logout_eng.php'>Log out</a></li>
                        
                     </ul>
                 </div>
@@ -62,8 +61,8 @@ if(!$fgmembersite->CheckLogin())
             <li><a href="#">Who we are?</a>
                 <div>
                     <ul>
-                        <li><a href="#">Our specialists</a></li>
-                        <li><a href="#">Concepts</a></li>
+                        <li><a href="../../../staff.html">Our specialists</a></li>
+                        <li><a href="../../../company.html">Concepts</a></li>
   
                     </ul>
                 </div>
@@ -89,7 +88,7 @@ if(!$fgmembersite->CheckLogin())
                     </ul>
                 </div>
             </li>
-            <li><a href="../../../contact.php">  Contact  </a></li>
+            <li><a href="../../../login-contact.php">  Contact  </a></li>
 			<li><a href="#">           </a></li>
 			
             <li class="pad"></li>
