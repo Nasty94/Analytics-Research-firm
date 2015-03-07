11133f1363adccde507b751deeb86a5cfbff7c4f
Anastassia <Nasty94@users.noreply.github.com>
2015-03-07 22:51:26 +0200

    add company.html and staff.html

--- source/login-contact.php
+++ source/login-contact.php
@@ -16,15 +16,15 @@ if(isset($_POST['submitted']))
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="et">
 <head>
     <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
-	<meta charset='utf-8'>
+    <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Minu konto</title>
      
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 
     <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
-	<script src="js/disable.js"></script>
+    <script src="js/disable.js"></script>
     <script src="scripts/pwdwidget.js" type="text/javascript"></script>   
 	
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
@@ -32,9 +32,9 @@ if(isset($_POST['submitted']))
 	<link rel="stylesheet" href="style/style.css">
 	<link rel="stylesheet" href="style/verticalmenu.css">
 	<link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
-    <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+        <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="STYLESHEET" type="text/css" href="style/fg_membersite_form.css" />
+        <link rel="STYLESHEET" type="text/css" href="style/fg_membersite_form.css" />
         
     	
 </head>
@@ -70,8 +70,8 @@ if(isset($_POST['submitted']))
 			<li><a href="#">Meist</a>
                 <div>
                     <ul>
-                        <li><a href="#">Personal</a></li>
-                        <li><a href="#">Concepts</a></li>
+                        <li><a href="staff.html">Personal</a></li>
+                        <li><a href="company.html">Ettevõtest</a></li>
   
                     </ul>
                 </div>
