9eabd2d32dc07789d46b06a74ce9b5121710e7da
Anastassia <Nasty94@users.noreply.github.com>
2015-03-11 23:40:29 +0200

    Update fg_membersite.php

--- source/include/fg_membersite.php
+++ source/include/fg_membersite.php
@@ -499,7 +499,7 @@ class FGMembersite
         
         $qry = "Update $this->tablename Set confirmcode='y' Where  confirmcode='$confirmcode'";
         
-        if(!mysqli_query( $qry ,$this->connection))
+        if(!mysqli_query($this->connection, $qry ))
         {
             $this->HandleDBError("Error inserting data to the table\nquery:$qry");
             return false;
