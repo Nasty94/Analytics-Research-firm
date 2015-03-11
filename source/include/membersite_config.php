d0135ceaf2c36f39f23c578d8afd56798ce4f47f
Anastassia <Nasty94@users.noreply.github.com>
2015-03-11 23:45:54 +0200

    Update membersite_config.php

--- source/include/membersite_config.php
+++ source/include/membersite_config.php
@@ -10,6 +10,8 @@ $fgmembersite->SetWebsiteName('consultingLK.com');
 //Provide the email address where you want to get notifications
 $fgmembersite->SetAdminEmail('Annett.Saarik@gmail.com');
 
+$fgmembersite->SetLeadAdminEmail('Anastassia.Ivanova.94@gmail.com');
+
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
