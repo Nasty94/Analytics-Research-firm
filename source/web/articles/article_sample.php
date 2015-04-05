b7401712feb2b53b38fa5a0d33acf4da2011a513
Markusli <markus.lippus@gmail.com>
2015-04-06 00:03:59 +0300

    Article sample page

--- source/web/articles/article_sample.php
+++ source/web/articles/article_sample.php
@@ -9,62 +9,76 @@ $has_comments = (count($comments) > 0);
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-
-	<meta charset="utf-8" />
-	
-	<link rel="stylesheet" href="../../style/comment-main.css" type="text/css" />
-	<meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
 	<meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Article sample</title>
+    <title>Minu konto</title>
      
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+    <script src="scripts/pwdwidget.js" type="text/javascript"></script>  
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
+	
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script type='text/javascript' src='../../scripts/gen_validatorv31.js'></script>
     <script src="../../scripts/pwdwidget.js" type="text/javascript"></script>   
 	
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
-	<link rel="stylesheet" href="../../style/menubar.css">
-	<link rel="stylesheet" href="../../style/style.css">
-	<link rel="stylesheet" href="../../style/verticalmenu.css">
+	<link rel="stylesheet" href="../../style/menubar_test.css">
+	<link rel="stylesheet" href="../../style/style_test.css">
 	<link rel="STYLESHEET" type="text/css" href="../../style/pwdwidget.css" />
-        <link rel="STYLESHEET" type="text/css" href="../../style/fg_membersite.css" />
+    <link rel="STYLESHEET" type="text/css" href="../../style/fg_membersite_test.css" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../../style/bootstrap.min.css">
+
+
         
+    	
 </head>
 
-<body id="index" class="home">
-    <div id="left"></div>
-		  <div id="right"></div>
-		  <div id="top"></div>
-		  <div id="bottom"></div>
-		  
-		  <div id="img">
-     		 
-		  <img src="../../img/LK.jpg" width=auto height=auto>
-		  </div <!-- img -->
-		  
-<div class="dropdownmenu">
-        <ul id="nav">
-            <li class='active'><a href='../../index.html'>Avaleht</a></li>
-            <li><a href="#">Minu konto</a>
+<body>
+    
+        <div class="container-fluid text-center">
+          <div class="row">
+             <div class="col-md-8 col-md-offset-2">
+                    <div id="english">
+		            <a id="eng" href="web/lang/eng/index_eng.html">ENG</a>&nbsp;&nbsp;
+	
+		  </div>
+             </div>
+           </div>
+           <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+		        <div id="img">
+                    <img src="../../img/LK.jpg" class="img-responsive" alt="img/LK.jpg"/>
+		        </div> 
+            </div>
+           </div>
+          <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+            <div class="dropdownmenu">
+            <ul id="nav">
+            <li class='active'><a href='index.html'>Avaleht</a></li>
+            <li><a href="login-home.php">Minu konto</a>
                 <div>
                     <ul>
-                        <li><a href='../../register.php'>Uus kasutaja</a></li>
-                        <li><a href='../../change-pwd.php'>Muuda parooli</a></li>
-			<li><a href='../../logout.php'>Logi välja</a></li>
+                        <li><a href='register.php'>Uus kasutaja</a></li>
+                        <li><a href='login.php'>Logi sisse</a></li>
                        
                     </ul>
                 </div>
             </li>
-            <li><a href="#">Meist</a>
+			<li><a href="#">Meist</a>
                 <div>
                     <ul>
-                        <li><a href="../../staff.html">Personal</a></li>
-                        <li><a href="../../company.html">Ettevõtest</a></li>
+                        <li><a href="#">Personal</a></li>
+                        <li><a href="#">Concepts</a></li>
   
                     </ul>
                 </div>
@@ -82,7 +96,7 @@ $has_comments = (count($comments) > 0);
             <li><a href="#">Blog</a>
                 <div>
                     <ul>
-                        <li><a href="#">Näidis</a></li>
+                        <li><a href="./web/articles/article_sample.php">Näidis</a></li>
                         <li><a href="#">Page 2</a></li>
                         <li><a href="#">Page 3</a></li>
                         <li><a href="#">Page 4</a></li>
@@ -90,24 +104,46 @@ $has_comments = (count($comments) > 0);
                     </ul>
                 </div>
             </li>
-            <li><a href="../../contact.php">  Kontakt  </a></li>
+            <li><a href="contact.php">  Kontakt  </a></li>
 			<li><a href="#">           </a></li>
-			
             <li class="pad"></li>
         </ul>
+          <div class="dropdown">
+            <button class="btn btn-success" type="button" id="menu1" data-toggle="dropdown">Menüü
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu dropdown-menu-right" role="menu">
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="index.html">Avaleht</a></li>
+                <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href='login'>Logi sisse</a></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href='register.php'>Uus kasutaja</a></li>
+                <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Meist</a></li>
+                <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Turu uuring</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Med statistika</a></li>
+                <li role="presentation" class="divider"></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="./web/articles/article_sample.php">Blogi</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Kontakt</a></li>
+              
+              
+            </ul>
+          </div>
     </div>
-       <br> <br> <br>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          	<div id="main">
+	            <div id="white-box" >
+	                        <div id="contentInt">
+                            <noscript>
+                                <p class="note">You have disabled Javascript. This website will not function without it.</p>
+                            </noscript>
 
-	
-	<section id="content" class="body">
 
-	 <div id="contentInt">
-                 <noscript>
-                        <p class="note">You have disabled Javascript. This website will not function without it.</p>
-                 </noscript>
-	<div class="center">
-	
-	  <div class="textart">
+
+			   		        <div class="center">
+                                   	  <div class="textart">
 	  <article class="hentry">	
 			<header>
 				<h2 class="entry-title"><a href="#" rel="bookmark" title="Permalink to this What You Need from your manager to be successful">What You Need from your manager to be successful</a></h2>
@@ -177,73 +213,27 @@ What will you gain?<br>
 	</div>
 			
 	</section>
-	
-	<section id="comments" class="body">
-	</div> <!--Center-->
-	</div> <!--contentInt-->
+                            </div><!--center-->
+                            </div> <!--contentInt-->
 		   		   
 
-
-	  
-	  <header>
-			<h2>Tagasiside</h2>
-		</header>
-
-    <ol id="posts-list" class="hfeed<?php echo($has_comments?' has-comments':''); ?>">
-      <li class="no-comments">Jaga oma arvamusega!</li>
-      <?php
-        foreach ($comments as &$comment) {
-          ?>
-          <li><article id="comment_<?php echo($comment['id']); ?>" class="hentry">	
-    				<footer class="post-info">
-    					<abbr class="published" title="<?php echo($comment['date']); ?>">
-    						<?php echo( date('d F Y', strtotime($comment['date']) ) ); ?>
-    					</abbr>
-
-    					<address class="vcard author">
-    						By <a class="url fn" href="#"><?php echo($comment['comment_author']); ?></a>
-    					</address>
-    				</footer>
-
-    				<div class="entry-content">
-    					<p><?php echo($comment['comment']); ?></p>
-    				</div>
-    			</article></li>
-          <?php
-        }
-      ?>
-		</ol>
-		
-		<div id="respond">
-
-      <h3>Mida Sina arvad artiklist?</h3>
-
-      <form action="../comment-sys/post_comment.php" method="post" id="commentform">
-
-        <label for="comment_author" class="required">Nimi</label>
-        <input type="text" name="comment_author" id="comment_author" value="" tabindex="1" required="required">
-        
-        <label for="email" class="required">Email</label>
-        <input type="email" name="email" id="email" value="" tabindex="2" required="required">
-
-        <label for="comment" class="required">Sinu arvamus</label>
-        <textarea name="comment" id="comment" rows="10" tabindex="4"  required="required"></textarea>
-
-        <input type="hidden" name="comment_post_ID" value="<?php echo($comment_post_ID); ?>" id="comment_post_ID" />
-        <input name="submit" type="submit" value="Submit " style="width:50p"/>
-        
-      </form>
-      
-    </div>
-			
-	</section>
-
+             
+	            </div> <!-- white box --> 
+	        </div> <!-- main -->
 	
-	<div id="footer" >
+        </div>
+   </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">		  
+	        <div id="footer" >
                       © 2015  LK Consulting <br>
 					  This is a proof-of-concept web application.
-	</div>
+	        </div>
+        </div>
+    </div>
 
 
-</body>
+    </div>
+        
+    </body>
 </html>
