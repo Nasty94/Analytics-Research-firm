82b3e0f7ad6bc83b4ae5e14fad66252c6743b185
Anastassia <Nasty94@users.noreply.github.com>
2015-04-14 22:12:44 +0300

    sth strange fixed

--- source/include/membersite_config.php
+++ source/include/membersite_config.php
@@ -15,14 +15,15 @@ $fgmembersite->SetAdminEmail('anastassia.ivanova.94@gmail.com');
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(
-//					  /*hostname*/'eu-cdbr-azure-north-b.cloudapp.net',
-                      /*hostname*/'%',
-//                      /*username*/'bc3106a32eb6a9',
-                      /*username*/'root2',
-//                      /*password*/'ff65da13',
-                      /*password*/'root2',
+                      /*hostname*/'eu-cdbr-azure-north-b.cloudapp.net',
+//                      /*hostname*/'%',
+                      /*username*/'bc3106a32eb6a9',
+//                      /*username*/'root2',
+                      /*password*/'ff65da13',
+//                      /*password*/'root2',
                       /*database name*/'lkconsult',
                       /*table name*/'users');
+
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
