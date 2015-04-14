a8cc3e2eb5ada89006ccd2dc5c63d8041d88f8ee
Nasty94 <Nasty94@hot.ee>
2015-04-14 22:52:13 +0300

    bootstrap???

--- source/admin_post_to_blog.php
+++ source/admin_post_to_blog.php
@@ -1,12 +1,12 @@
 <?php
-require('web/comment-sys/Persistence.php');
+require('./web/comment-sys/Persistence.php');
 $comment_post_ID = 1;
 $db = new Persistence();
 $comments = $db->get_comments($comment_post_ID);
 $has_comments = (count($comments) > 0);
 
-require_once('include/fg_membersite.php');
-require_once('include/membersite_config.php');
+require_once('./include/fg_membersite.php');
+require_once('./include/membersite_config.php');
 
 
 if($fgmembersite->Login()){
@@ -24,62 +24,73 @@ if($fgmembersite->Login()){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-
-	<meta charset="utf-8" />
-	
-	<link rel="stylesheet" href="style/comment-main.css" type="text/css" />
-	<meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
-	<meta charset='utf-8'>
+    <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
+    <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Article sample</title>
+    <title>Post to blog</title>
      
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
     <script src="scripts/pwdwidget.js" type="text/javascript"></script>   
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
 	
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
 	<link rel="stylesheet" href="style/menubar.css">
 	<link rel="stylesheet" href="style/style.css">
 	<link rel="stylesheet" href="style/verticalmenu.css">
 	<link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
-        <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+    <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/bootstrap-theme.css">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">    
 </head>
 
-<body id="index" class="home">
-    <div id="left"></div>
-		  <div id="right"></div>
-		  <div id="top"></div>
-		  <div id="bottom"></div>
-		  
-		  <div id="img">
-     		 
-		  <img src="img/LK.jpg" width=auto height=auto>
-		  </div <!-- img -->
-		  
-<div class="dropdownmenu">
-        <ul id="nav">
-            <li class='active'><a href='index.html'>Avaleht</a></li>
-            <li><a href="#">Minu konto</a>
+<body>
+    
+        <div class="container-fluid text-center">
+          <div class="row">
+             <div class="col-md-8 col-md-offset-2">
+                    <div id="english">
+		            <a id="eng" href="web/lang/eng/index_eng.html">ENG</a>&nbsp;&nbsp;
+	
+		  </div>
+             </div>
+           </div>
+           <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+		        <div id="img">
+                    <img src="img/LK.jpg" class="img-responsive" alt="img/LK.jpg"/>
+		        </div> 
+            </div>
+           </div>
+          <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+            <div class="dropdownmenu">
+            <ul id="nav">
+            <li class='active'><a href='admin_index.html'>Avaleht</a></li>
+            <li><a href="login-home_bootstrap.php">Minu konto</a>
                 <div>
                     <ul>
-                        <li><a href='register.php'>Uus kasutaja</a></li>
+                        <li><a href='clients_data.php'>Minu andmed</a></li>
+                        <li><a href='all_orders.php'>Tellimuste ajalugu</a></li>
+                        <li><a href='all_users.php'>Klientide kontod</a></li>
                         <li><a href='change-pwd.php'>Muuda parooli</a></li>
 			<li><a href='logout.php'>Logi välja</a></li>
                        
                     </ul>
                 </div>
             </li>
-            <li><a href="#">Meist</a>
+			<li><a href="#">Meist</a>
                 <div>
                     <ul>
                         <li><a href="staff.html">Personal</a></li>
-                        <li><a href="company.html">Ettevõtest</a></li>
+                        <li><a href="company.html">Concepts</a></li>
   
                     </ul>
                 </div>
@@ -97,7 +108,14 @@ if($fgmembersite->Login()){
             <li><a href="#">Blog</a>
                 <div>
                     <ul>
-                        <li><a href="#">Näidis</a></li>
+                        <li><a href="admin_post_to_blog.php">Postita blogi</a></li>
+                        
+  
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="./web/articles/article_sample.php">Näidis</a></li>
                         <li><a href="#">Page 2</a></li>
                         <li><a href="#">Page 3</a></li>
                         <li><a href="#">Page 4</a></li>
@@ -107,20 +125,47 @@ if($fgmembersite->Login()){
             </li>
             <li><a href="contact.php">  Kontakt  </a></li>
 			<li><a href="#">           </a></li>
-			
             <li class="pad"></li>
         </ul>
+          <div class="dropdown">
+            <button class="btn btn-success" type="button" id="menu1" data-toggle="dropdown">Menüü
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu dropdown-menu-right" role="menu">
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="bootstrap_test.html">Avaleht</a></li>
+                <li role="presentation" class="divider"></li>
+                        <li><a href='clients_data.php'>Minu andmed</a></li>
+                        <li><a href='all_orders.php'>Tellimuste ajalugu</a></li>
+                        <li><a href='all_users.php'>Klientide kontod</a></li>
+                        <li><a href='change-pwd.php'>Muuda parooli</a></li>
+			            <li><a href='logout.php'>Logi välja</a></li>
+                <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Meist</a></li>
+                <li role="presentation" class="divider"></li>
+              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Turu uuring</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Med statistika</a></li>
+                <li role="presentation" class="divider"></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="./web/articles/article_sample.php">Blogi</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Kontakt</a></li>
+              
+              
+            </ul>
+          </div>
     </div>
-       <br> <br> <br>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          	<div id="main">
+                <h2>Tere, administraator!</h2>
+	            <div id="white-box" >
+	                        <div id="contentInt">
+                            <noscript>
+                                <p class="note">You have disabled Javascript. This website will not function without it.</p>
+                            </noscript>
+                            <h1></h1>
 
-	
-	<section id="content" class="body">
 
-	 <div id="contentInt">
-                 <noscript>
-                        <p class="note">You have disabled Javascript. This website will not function without it.</p>
-                 </noscript>
-	<div class="center">
+			   		        <div class="center">
 	
 	  <div class="textart">
 	  <article class="hentry">	
@@ -225,11 +270,14 @@ Content
 			
 	</section>
 
-	
-	<div id="footer" >
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">		  
+	        <div id="footer" >
                       © 2015  LK Consulting <br>
 					  This is a proof-of-concept web application.
-	</div>
+	        </div>
+        </div>
+    </div>
 
 
 </body>
