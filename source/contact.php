d4155a84cffa4caf01e576834b3a14b5ee2155fa
Markusli <markus.lippus@gmail.com>
2015-04-21 14:42:01 +0300

    Fixing html errors

--- source/contact.php
+++ source/contact.php
@@ -15,7 +15,7 @@ if(isset($_POST['submitted']))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-    <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1" />
 	<meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -57,8 +57,7 @@ if(isset($_POST['submitted']))
 		  <div id="english">
 		  <br>
 		  <a id="eng" href="web/lang/eng/index_eng.html">ENG</a>&nbsp;&nbsp;
->>>>>>> 32420bb182cc287ea2bfe89d205b8d121e40443e
-	
+
 		  </div>
              </div>
            </div>
@@ -114,7 +113,7 @@ if(isset($_POST['submitted']))
                     </ul>
                 </div>
             </li>
-            <li><a href="contact_bootstrap.php">  Kontakt  </a></li>
+            <li><a href="contact.php">  Kontakt  </a></li>
 			<li><a href="#">           </a></li>
             <li class="pad"></li>
         </ul>
