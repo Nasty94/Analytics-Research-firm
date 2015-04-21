a39ab9a03f6498e95efd119f0bb5b9f375651b8c
Markusli <markus.lippus@gmail.com>
2015-04-21 14:45:02 +0300

    Html errors

--- source/contact.php
+++ source/contact.php
@@ -12,13 +12,13 @@ if(isset($_POST['submitted']))
 ?>
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
     <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1" />
 	<meta charset='utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Minu konto</title>
      
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
