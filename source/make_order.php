d8db2de0fb31f6da364c7480a500b89c2f335e7a
annetts <Annett.Saarik@hotmail.com>
2015-04-23 20:20:37 +0300

    proov

--- source/make_order.php
+++ source/make_order.php
@@ -22,7 +22,7 @@ if(isset($_POST['submitted']))
 
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US" manifest="manifest.appcache">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US" manifest="/source/manifest.appcache">
 
 <head>
     <meta http-equiv="Content type" content="text/html; charset=ISO-8859-1">
@@ -33,7 +33,7 @@ if(isset($_POST['submitted']))
      
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
-    <script type='text/javascript' src='/offline.js'></script>
+    <script type='text/javascript' src='offline.js'></script>
     <script src="scripts/pwdwidget.js" type="text/javascript"></script>   
 	
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
