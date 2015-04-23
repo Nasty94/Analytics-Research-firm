cf4bf83d28e2dcd1d28f5bb3dbfbdd73d11b42ba
annetts <Annett.Saarik@hotmail.com>
2015-04-23 17:02:13 +0300

    proov

--- source/make_order.php
+++ source/make_order.php
@@ -1,3 +1,4 @@
+<!--
 <?PHP
 
 require_once("./include/membersite_config.php");
@@ -17,7 +18,7 @@ require_once("./include/membersite_config.php");
  }
 
 ?>
-
+-->
 <!--
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
