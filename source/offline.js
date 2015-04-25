cffa790e495212dd315b226a6ace115e2e97d554
annetts <Annett.Saarik@hotmail.com>
2015-04-25 14:44:48 +0300

    Offline parandused

--- source/offline.js
+++ source/offline.js
@@ -14,9 +14,11 @@ else {
 
 });
 
+/*
 function isOnLine() {
     return navigator.onLine;
 }
+*/
 
 function getModel(index) {
     var model = {
@@ -113,3 +115,23 @@ reportOnlineStatus();
 document.addEventListener("load", function (e) {
 reportOnlineStatus();
 }, true);
+
+function isOnLine() {
+    var xhr = new XMLHttpRequest();
+    var file = "http://lk-consulting.azurewebsites.net/source/img/LK.jpg";
+    var randomNum = Math.round(Math.random() * 10000);
+     
+    xhr.open('HEAD', file + "?rand=" + randomNum, false);
+     
+    try {
+        xhr.send();
+         
+        if (xhr.status >= 200 && xhr.status < 304) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
