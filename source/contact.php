687b6d484d8008310da3e21e39845d158724a070
Markusli <markus.lippus@gmail.com>
2015-04-25 14:36:23 +0300

    Separated ajax from html

--- source/contact.php
+++ source/contact.php
@@ -203,32 +203,7 @@
 
     </div>
 
-        <script>
-            $(document).ready(function () {
-                $('#contactform').submit(function (event) {
-
-                    sendContactForm();
-                    return false;
-                });
-
-
-                function sendContactForm() {
-                    $.ajax({
-                        type: "POST",
-                        url: 'ajaxphp.php',
-                        data: $("#contactform").serialize(),
-
-                        success: function (errorMessage) {
-                            alert("Sõnum saadetud!\n" + "Võtame teiega peagi ühendust.");
-                        },
-                        error: function (data) {
-                            alert("Midagi läks valesti  " + data.msg);
-                        }
-                    });
-
-                }
-            });
-        </script>
+       <SCRIPT SRC="./js/ajax.js"></SCRIPT>>
         
     </body>
 </html>
