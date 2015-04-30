79fa99f4e481e5b3ccea8918dfa02d5d1b987e45
Anastassia Ivanova <Nasty94@users.noreply.github.com>
2015-04-30 20:26:51 +0300

    fix links

--- source/login.php
+++ source/login.php
@@ -84,7 +84,7 @@ elseif( isset( $_REQUEST["provider"] ) )
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-	<meta charset='utf-8'>
+    <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
      
@@ -93,17 +93,15 @@ elseif( isset( $_REQUEST["provider"] ) )
     <script src="scripts/pwdwidget.js" type="text/javascript"></script>  
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>      
 	
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
-	<link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" type="text/css">
+    <link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
     <link rel="STYLESHEET" type="text/css" href="style/fg_membersite_test.css" />
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style/menubar_test.css" type="text/css" media="all">
     <link rel="stylesheet" href="style/style_test.css" type="text/css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/bootstrap-theme.css">
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/bootstrap-theme.css.map">
     
-  
 </head>
     
     <body> 
