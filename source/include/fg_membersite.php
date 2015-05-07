f36962397b66aff1df8edd223fc720aa266cb74f
Anastassia Ivanova <Nasty94@users.noreply.github.com>
2015-05-07 19:00:01 +0300

    remove wrong SendContactEmail function

--- source/include/fg_membersite.php
+++ source/include/fg_membersite.php
@@ -811,126 +811,8 @@ class FGMembersite
  
     }
 	
-	function SendContactEmail()
-    {
-	if(isset($_POST['email'])) {
- 
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "anastassia.ivanova.94@gmail.com";
- 
-    $email_subject = "New message from user";
-    
-	died($error)
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['last_name']) ||
- 
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
- 
-        !isset($_POST['comments'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');      
- 
-    }
- 
-     
- 
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
- 
-    $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
- 
-    $comments = $_POST['comments']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_to."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers); 
 
-}
-    function SendContactEmail2()
+    function SendContactEmail()
     {
 		require 'PHPMailerAutoload.php';
 		$mailer = new PHPMailer;
@@ -947,15 +829,15 @@ $headers = 'From: '.$email_from."\r\n".
 		$mailer->Password = 'lkconsulting';
         $mailer->addReplyTo('lkcmailer@gmail.com', 'First Last');  
         
-        $mailer->AddAddress($_POST['email'],$_POST['first_name']);
+        $mailer->AddAddress('anastassia.ivanova.94@gmail.com','Anastassia Ivanova');
         
         //$mailer->Subject = "Your registration with ".$this->sitename;
 
-        $mailer->setFrom($this->GetFromAddress(),"lkcmailer");   
-		$mailer->From ='LKCmailer';
+        $mailer->setFrom($_POST['email'],$_POST['first_name']);   
+		$mailer->From =$_POST['first_name'];
         $mailer->Subject = 'Saabunud kommentaar lehelt '.$this->sitename;
         
-        $mailer->Body ="Hello ".$_POST['first_name']."\r\n\r\n".
+        $mailer->Body ="Tere ".$_POST['first_name']."\r\n\r\n".
         "Teile on saabunud kommentaar lehelt ".$this->sitename."\r\n\r\n".
         $_POST['comments']."\r\n".
         "\r\n".
